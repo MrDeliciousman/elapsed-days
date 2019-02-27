219b5045c384442d2b6af62ebda7a4f21b90a4e3
Thomas Herrera <rayherrera90@outlook.com>
2019-02-26 19:34:47 -0700

    Added method.

--- src/edu/cnm/deepdive/util/DateOnly.java
+++ src/edu/cnm/deepdive/util/DateOnly.java
@@ -3,7 +3,26 @@ package edu.cnm.deepdive.util;
 public class DateOnly {
 
   public static int elapsedDays(int year, int month, int day) {
-    return 0; // TODO Implement this method to return correct value.
+    int count = 0;
+    int[] daysinMonth = {31, 28, 31, 30,31, 30, 31, 31,30, 31, 30, 31};
+    count = year - 1970;
+    count += day;
+    for (int i = 0; i <= month; i++) {
+      count += daysinMonth[i];
+    }
+    for (int i = 1970; i <= year; i++) {
+      isLeapYear(i);
+    }
+
+    return count; // TODO Implement this method to return correct value.
+  }
+
+  private static boolean isLeapYear(int year) {
+    if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+
+      return true;
+    }
+    return false;
   }
 
 }
