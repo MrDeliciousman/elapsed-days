b251059fab0180d7a0d4d77fb999e484901a0d15
Thomas Herrera <rayherrera90@outlook.com>
2019-02-26 22:08:07 -0700

    Fixed method to Pass norm test.

--- src/edu/cnm/deepdive/util/DateOnly.java
+++ src/edu/cnm/deepdive/util/DateOnly.java
@@ -1,28 +1,45 @@
 package edu.cnm.deepdive.util;
 
+import java.text.DateFormat;
+
 public class DateOnly {
 
   public static int elapsedDays(int year, int month, int day) {
     int count = 0;
-    int[] daysinMonth = {31, 28, 31, 30,31, 30, 31, 31,30, 31, 30, 31};
-    count = year - 1970;
-    count += day;
-    for (int i = 0; i <= month; i++) {
-      count += daysinMonth[i];
+    int[] daysInMonth = {31, 28, 31, 30,31, 30, 31, 31,30, 31, 30, 31};
+    int START_YEAR = 1970;
+    count = (year - 1970) * 365;
+    count += (day - 1);
+    if(year<START_YEAR){
+      START_YEAR = year;
+      year = 1970;
     }
-    for (int i = 1970; i <= year; i++) {
-      isLeapYear(i);
+    if (month > 1) {
+      for (int i = 0; i < month; i++) {
+        count += daysInMonth[i];
+      }
     }
+      for (int i = START_YEAR; i <= year; i++) {
+        if (isLeapYear(i)) {
+          if (year == 1970) {
+            count--;
+          } else {
 
-    return count; // TODO Implement this method to return correct value.
+            count++;
+          }
+        }
+      }
+
+    return count;
   }
 
   private static boolean isLeapYear(int year) {
     if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
 
       return true;
+    } else {
+      return false;
     }
-    return false;
   }
 
 }
